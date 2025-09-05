d76701234e090b1e4b29b67ba3960fff0f9555bd
KarimHazem186 <karimhazem186@gmail.com>
2025-09-05 09:43:48 +0300

    Update connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
 
-// const DB = 'mongodb+srv://mern-auth:mern1234@mern-auth.embzx.mongodb.net/Amazonweb?retryWrites=true&w=majority&appName=MERN-Auth'
 const DB = process.env.DATABASE
 mongoose.connect(DB).then(()=>console.log("database connected")).catch((error)=>console.log("error ",+error.message));
